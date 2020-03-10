bc316b9939cfe5367af57f58c47f58a4b26ba683
scorpion108 <59804467+scorpion108@users.noreply.github.com>
2020-03-10 21:04:04 +0200

    Add project files.

---
+++ ConsoleApp5/Node.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Node<T>
+    {
+        private T Value;
+        private Node<T> next;
+
+        public Node(T Value)
+        {
+            this.Value = Value;
+            this.next = null;
+        }
+
+        public Node(T Value, Node<T> next)
+        {
+            this.Value = Value;
+            this.next = next;
+        }
+
+        public T GetValue()
+        {
+            return Value;
+        }
+
+        public Node<T> GetNext()
+        {
+            return next;
+        }
+
+        public void SetValue(T Value)
+        {
+            this.Value = Value;
+        }
+
+        public void SetNext(Node<T> next)
+        {
+            this.next = next;
+        }
+
+        public override string ToString()
+        {
+            return this.Value.ToString();
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static int NodeCounter(Node<int> h)
+        {
+            int count = 0;
+            Node<int> temp = h;
+            while(temp.GetNext()!= null)
+            {
+                temp = temp.GetNext();
+                count++;
+            }
+            return count;
+        }
+
+       public static bool IsSection(Node<int> h)
+        {
+            Node<int> temp = h;
+            if (temp == null)
+                return true;
+            temp = temp.GetNext();
+            //int FullLength = NodeCounter(h);
+            //int Length = NodeCounter(temp);
+            for (int i = 0; i < NodeCounter(h); i++) //   הייתי צריך לעשות משתנה לאורך של המערך ואת האורך של המערך פחות אחד במקום כל פעם לחפש אותו כמו מלמעלה 
+            {
+                for (int j = 0; j < NodeCounter(temp); j++)
+                {
+                    if (h.GetValue() > temp.GetValue())
+                        return false;
+                }
+                h = h.GetNext();
+                temp = temp.GetNext();
+            }
+            return true;    //i didnt do it
+        }
+
+        public static void DeleteDeffect(Node<int> h)
+        {
+            while (h != null)
+            {
+                if (h.GetValue() > (NodeCounter(h) ))   //ERROR
+                {
+                    if (h.GetNext().GetNext() != null)  //ERROR
+                        h.SetNext(h.GetNext().GetNext());   //ERROR
+                    else
+                        h.SetNext(null);    //ERROR 
+                }
+                h = h.GetNext();    //ERROR
+            }
+
+        }
+
+
+        
+    }
+}
